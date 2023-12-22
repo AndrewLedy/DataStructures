849eb7198f95c30629eaa9f96d62e3c7ae9cd0a6
Andrew Ledy <andrew.programing@gmail.com>
2023-12-22 17:47:08 +0000

    Misssed to add git ignore. Gonna add part of this commit.

---
+++ DataStructures/CustomArray.cs
@@ -0,0 +1,54 @@
+﻿namespace DataStructures
+{
+    public class CustomArray
+    {
+        private int[] Items { get; set; }
+        private int Count { get; set; }
+        public CustomArray(int length = 3)
+        {
+            Items= new int[length];
+            Count = 0;
+        }
+
+        public int[] Insert(int value)
+        {
+            if (Count == Items.Length)
+            {
+                int[] newItems = new int[Count * 2];
+                for (int i = 0; i < Count; i++) //Why not length?
+                {
+                    newItems[i] = Items[i];
+                }
+
+                newItems[Count++] = value;
+                Items= newItems;
+            }
+            else
+            {
+                Items[Count++] = value;               
+            }
+            return Items;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index<0 || index>=Count)
+            {
+                throw new InvalidDataException();
+            }
+            for (int i=index; i<Count; i++)
+            {
+                Items[i] = Items[i + 1];
+            }
+            Count--;
+        }
+
+        public void Print()
+        {
+            for (int i=0;i<Count;i++)
+            {
+                Console.WriteLine(Items[i]);
+            }
+        }
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructures;
+
+Console.WriteLine("Hello, World!");
+CustomArray array = new CustomArray(2);
+array.Print();
+array.Insert(1);
+array.Insert(2);
+array.Print();
+array.Insert(3);
+array.Insert(4);
+array.Insert(5);
+array.Print();
+array.RemoveAt(4);
+array.Print();
+Console.ReadLine();
